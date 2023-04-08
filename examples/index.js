58cd0ac684bd50721e9ee068554b84feddfcb25b
Safet Zahirovic <zahsaf@Safets-MacBook-Air.local>
2023-04-08 19:02:49 +0200

    Add config for Render deployment in examples

--- examples/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import router from "./router.js";
-const app = express();
-const port = 4000;
-app.use(router);
-app.listen(port, () => {
-    console.log(`App is listening on port ${port} !`);
-});
--- examples/public/middlewares.js
+++
@@ -1,5 +0,0 @@
-export const headerMiddleware = (req, res, next) => {
-    res.setHeader("Content-Type", "text/html");
-    res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-    next();
-};
